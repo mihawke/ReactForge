003fcc60a00477bdc28cb4b35fa35edd9f04fc35
mihawke <pratikgawari0000@gmail.com>
2024-05-04 21:41:27 +0530

    Added Files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+      
+    </div>
+  )
+}
+
+export default App
